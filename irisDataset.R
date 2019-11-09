e927bf9449f2468fa9ab7225c33bce0a5a245cc1
nikitapitliya006 <nikitapitliya006@gmail.com>
2019-11-09 21:41:23 +0000

    knn

--- irisDataset.R
+++ irisDataset.R
@@ -1,13 +1,37 @@
 library(datasets)
-data(iris)
-summary(iris)
+df <- data(iris) ##load data
+head(iris) ## see the studcture
 
-#install.packages("dplyr")
-names(iris) <- tolower(names(iris))
-library(dplyr)
+##Generate a random number that is 90% of the total number of rows in dataset.
+ran <- sample(1:nrow(iris), 0.9 * nrow(iris)) 
 
-virginica <- filter(iris, species == "virginica")
-head(virginica) # This dispalys the first six rows
+##the normalization function is created
+nor <-function(x) { (x -min(x))/(max(x)-min(x))   }
 
-sepalLength6 <- filter(iris, species == "virginica", sepal.length > 6)
-tail(sepalLength6) # compare this to head()
+##Run nomalization on first 4 coulumns of dataset because they are the predictors
+iris_norm <- as.data.frame(lapply(iris[,c(1,2,3,4)], nor))
+
+summary(iris_norm)
+
+##extract training set
+iris_train <- iris_norm[ran,] 
+##extract testing set
+iris_test <- iris_norm[-ran,]
+
+##extract 5th column of train dataset because it will be used as 'cl' argument in knn function.
+iris_target_category <- iris[ran,5]
+##extract 5th column if test dataset to measure the accuracy
+iris_test_category <- iris[-ran,5]
+
+##load the package class
+library(class)
+##run knn function
+pr <- knn(iris_train,iris_test,cl=iris_target_category,k=13)
+
+##create confusion matrix
+tab <- table(pr,iris_test_category)
+
+##this function divides the correct predictions by total number of predictions that tell us how accurate teh model is.
+
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+accuracy(tab)
